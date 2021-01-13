d9c5e4f456bd5d47ff9bb0836679f4d709783767
tlpackus <tlpackus@gmail.com>
2021-01-12 20:28:40 -0800

    add buttons that make alerts and scrips come up on screen that you control

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      alert('hi');
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi');
+    });
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      alert('bye');
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert('bye');
+    });
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+  });
+});
+
+
